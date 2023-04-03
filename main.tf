98875e83e312ed94979ead0f96c1dc0eb2a7fbaa
venkat <venkat.mobiqa@gmail.com>
2023-04-03 07:27:54 +0530

    updating ./tf-module-docdb1

--- main.tf
+++ main.tf
@@ -66,3 +66,21 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
     { Name = "${var.env}-docdb-cluster-instance-${count.index + 1}" }
   )
 }
+
+resource "aws_ssm_parameter" "docdb_url_catalogue" {
+  name  = "${var.env}.catalogue.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.DB_ADMIN_USER.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name  = "${var.env}.user.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.DB_ADMIN_USER.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url" {
+  name  = "${var.env}.docdb.DOCDB_URL"
+  type  = "String"
+  value = aws_docdb_cluster.docdb.endpoint
+}
